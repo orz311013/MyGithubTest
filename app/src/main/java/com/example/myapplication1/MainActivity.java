3a26d2b32af602032d618f17e798cc95786bbba0
orz311013@gmail.com <a23925487>
2019-05-26 20:43:58 +0800

    v1

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.v("test","test");
-        Log.v("test","test");
+        Log.v("test2","test2");
     }
 }
