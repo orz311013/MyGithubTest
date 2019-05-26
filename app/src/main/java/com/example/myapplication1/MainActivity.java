c3b619ac51654c5c09b3807029071c4f7a81538e
orz311013@gmail.com <a23925487>
2019-05-26 20:48:42 +0800

    v3

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -3,14 +3,25 @@ package com.example.myapplication1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-                Log.v("test2","test2");
-        Log.v("test3","test3");
+        Log.v("test2", "test2");
+        Log.v("test3", "test3");
+
     }
+
+    public void btn(View v) {
+        Toast.makeText(MainActivity.this, "TEST", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
